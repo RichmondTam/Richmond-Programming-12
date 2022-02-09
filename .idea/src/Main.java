6dc0961fe27ddffff6bed115b0af95420fc46c6e
RichmondTam <tampokman001@gmail.com>
2022-02-08 20:48:16 -0800

    Assignment 1

---
+++ .idea/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String q1 = "1+1\n"
+                + "(a)2 (b)4";
+        String q2 ="2+2\n"
+                +"(a)4 (b)6";
+        Question[] questions = {
+                new Question(q1, "a"),
+                new Question(q2,"a" )
+        };
+        runTest(questions);
+
+
+    }
+
+    public static void runTest(Question[] questions) {
+        Scanner keyboardInput = new Scanner(System.in);
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i].quiz);
+            String answer = keyboardInput.nextLine();
+            if (answer.equals(questions[i].answer)) {
+                System.out.println("Correct");
+            }
+            else{
+                System.out.println("Wrong");
+            }
+
+
+        }
+    }
+}
---
+++ .idea/src/Question.java
@@ -0,0 +1,9 @@
+public class Question {
+    String quiz;
+    String answer;
+
+    public Question(String quiz, String answer){
+        this.quiz=quiz;
+        this.answer=answer;
+    }
+}
