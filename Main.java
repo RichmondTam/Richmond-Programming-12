d02ed4b9658764bb5821dd416c3c5a98643c3257
RichmondTam <99051937+RichmondTam@users.noreply.github.com>
2022-03-31 12:02:50 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,9 +10,11 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-
+        //read text file
         String illiad= Files.readString(Path.of("C:\\Users\\2474710\\Downloads\\illiad.txt"));
+        //filter and replace words
         String[] words = illiad.toLowerCase().replaceAll("[^a-zA-Z1-9\\-' ]"," ").split(" ");
+        //filter unique words
         HashSet<String> uniqueWords = new HashSet<>(Arrays.asList(words));
 
 
