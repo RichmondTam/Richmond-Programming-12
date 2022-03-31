d36d6c94379dca6714f0bf6d1eab0f40cec9438a
RichmondTam <99051937+RichmondTam@users.noreply.github.com>
2022-03-31 11:39:33 -0700

    Update CaesarCipher.java

--- CaeserCipher/CaesarCipher.java
+++ CaeserCipher/CaesarCipher.java
@@ -8,7 +8,7 @@ public class CaesarCipher {
     public char letters[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     HashMap<Character, Integer> map;
 
-    //screate Map
+    //create Map
     public CaesarCipher(){
         map= new HashMap<>();
         for(int i=0;i<25;i++){
