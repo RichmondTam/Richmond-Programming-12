0fbf3dbd69147cfb0998c098874d581d7e7f8255
RichmondTam <99051937+RichmondTam@users.noreply.github.com>
2022-04-01 11:36:07 -0700

    Update Main.java

--- CaeserCipher/Main.java
+++ CaeserCipher/Main.java
@@ -3,7 +3,7 @@ package CaeserCipher;
 public class Main {
     public static void main(String[] args) {
         CaesarCipher caesarCipher= new CaesarCipher();
-        System.out.println(caesarCipher.encrypt("a",2));
-        System.out.println(caesarCipher.decrypt("a",2));
+        System.out.println(caesarCipher.encrypt("abc",2));
+        System.out.println(caesarCipher.decrypt("abc",2));
     }
 }
