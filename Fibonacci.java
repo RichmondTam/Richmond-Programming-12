2f12b38b535477493c8436c39b6efa729671eddf
RichmondTam <99051937+RichmondTam@users.noreply.github.com>
2022-04-02 22:08:16 -0700

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+public class Fibonacci {
+    static Integer[]numbers={1,1,2,3,5,8,13,21,34};
+
+
+    public static int Fibonacci(Integer[] numbers, int n){
+
+        if(n<=0){
+            return 0;
+        }
+        else{
+            return  Fibonacci(numbers, n-1 ) + numbers[n-1];
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Fibonacci(numbers,5));
+    }
+
+}
