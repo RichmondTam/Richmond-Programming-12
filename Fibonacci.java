b2ed7274c0f3d2f1845a8a239f17ccc51f5998ad
RichmondTam <99051937+RichmondTam@users.noreply.github.com>
2022-04-02 22:16:38 -0700

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,7 +1,7 @@
 public class Fibonacci {
     static Integer[]numbers={1,1,2,3,5,8,13,21,34};
 
-
+    //recursive method to add terms before
     public static int Fibonacci(Integer[] numbers, int n){
 
         if(n<=0){
